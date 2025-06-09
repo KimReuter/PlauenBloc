496b578d67beca141784d9be43329e00b4eae1eb
Kim Reuter <kimbaerlyyo@web.de>
2025-06-09 20:25:56 +0200

    set up authViewModel

--- shared/src/commonMain/kotlin/com/example/plauenblod/di/AppModule.kt
+++ shared/src/commonMain/kotlin/com/example/plauenblod/di/AppModule.kt
@@ -2,6 +2,9 @@ package com.example.plauenblod.di
 
 import com.example.plauenblod.data.auth.AuthRepository
 import com.example.plauenblod.data.auth.FirebaseAuthRepository
+import com.example.plauenblod.viewmodel.AuthViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 val appModule = module {
@@ -9,5 +12,11 @@ val appModule = module {
     single<AuthRepository> {
         FirebaseAuthRepository()
     }
+
+    single<CoroutineScope> {
+        CoroutineScope(Dispatchers.Main)
+    }
+
+    single { AuthViewModel(get(), get()) }
 
 }
--- shared/src/commonMain/kotlin/com/example/plauenblod/viewmodel/AuthViewModel.kt
+++ shared/src/commonMain/kotlin/com/example/plauenblod/viewmodel/AuthViewModel.kt
@@ -1,4 +1,39 @@
 package com.example.plauenblod.viewmodel
 
-class AuthViewModel {
+import com.example.plauenblod.data.auth.AuthRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class AuthViewModel(
+    private val authRepository: AuthRepository,
+    private val coroutineScope: CoroutineScope
+) {
+    val isLoggedIn: StateFlow<Boolean> = authRepository.isLoggedIn
+
+    fun signIn(email: String, password: String, onError: (Throwable) -> Unit = {}) {
+        coroutineScope.launch {
+            authRepository.signIn(
+                email = email,
+                password = password,
+                onSuccess = { /* Logging oder Navigation */ },
+                onError = onError
+            )
+        }
+    }
+
+    fun signUp(email: String, password: String, onError: (Throwable) -> Unit = {}) {
+        coroutineScope.launch {
+            authRepository.signUp(
+                email = email,
+                password = password,
+                onSuccess = { /* direkt einloggen */},
+                onError = onError
+            )
+        }
+    }
+
+    fun signOut() {
+        authRepository.signOut()
+    }
 }
