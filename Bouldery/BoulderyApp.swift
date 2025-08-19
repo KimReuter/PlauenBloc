328e305aeb9c85fbfddd817625536e3a861db6c1
Kim Reuter <kimbaerlyyo@web.de>
2025-08-19 11:33:50 +0200

    Initial Commit

---
+++ Bouldery/BoulderyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoulderyApp.swift
+//  Bouldery
+//
+//  Created by Kim Reuter on 19.08.25.
+//
+
+import SwiftUI
+
+@main
+struct BoulderyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
